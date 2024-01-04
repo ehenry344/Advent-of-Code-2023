e9445377a7a530bcdbfee23f433097bacbc58555
Evan Henry <68968256+ehenry344@users.noreply.github.com>
2024-01-04 16:16:16 -0500

    Update Day8.py
    P2 Solved + Memory Optimization

--- Day8.py
+++ Day8.py
@@ -1,4 +1,5 @@
 import re 
+from math import lcm 
 
 def parse_input(input_path): 
     with open(input_path, "r") as input_obj: 
@@ -16,44 +17,36 @@ def parse_input(input_path):
             adj_list[c_parsed[0]] = c_parsed[1:]
         
         return pos_sequence, adj_list 
+
+def get_zpath_len(start_node, end_rule, turns, adj_l): 
+    path_len = 0 
     
-def get_zpath(start_node, turns, adj_list, end_rule): 
-    move_index = 0
-    path = [start_node] 
+    move_index = 0 
+    current_node = start_node
 
-    while not end_rule(path[-1]): 
-        path.append(adj_list[path[-1]][turns[move_index]]) 
+    while not end_rule(current_node): 
+        current_node = adj_l[current_node][turns[move_index]] 
 
-        move_index += 1 
+        move_index += 1
+        if move_index == len(turns): move_index = 0
 
-        if move_index == len(turns): 
-            move_index = 0 
-
-    return path
+        path_len += 1 
+    
+    return path_len 
 
 def solve_p1(turns, adj): 
-    return len(get_zpath("AAA", turns, adj, lambda n: n == "ZZZ"))-1
+    return get_zpath_len("AAA", lambda n: n == "ZZZ", turns, adj)
 
 def solve_p2(turns, adj): 
     path_lens = [] 
 
     for node in adj.keys(): 
         if node[-1] == "A": 
-            z_path = get_zpath(node, turns, adj, lambda n: n[-1] == "Z")
+            path_len = get_zpath_len(node, lambda n: n[-1] == "Z", turns, adj) 
+            path_lens.append(path_len) 
 
-            path_lens.append(len(z_path)-1) 
-
-    min_path = min(path_lens)-1
-    c_product = 1
-    print(path_lens) 
-    print(min_path) 
-    for p in path_lens: 
-        c_product *= (p - min_path) 
-
-    return min_path + sum(path_lens) 
-
+    return lcm(*path_lens)
 
 turns, adj = parse_input("./Input/Day8.txt") 
-# current answer : too low 
-#print("Part 1 Solution:", solve_p1(turns, adj)) 
+print("Part 1 Solution:", solve_p1(turns, adj)) 
 print("Part 2 Solution:", solve_p2(turns, adj)) 
