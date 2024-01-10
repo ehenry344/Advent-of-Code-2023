d94a692109a2e954653a4d443e69acb5376ab47f
Evan Henry <68968256+ehenry344@users.noreply.github.com>
2024-01-09 21:35:35 -0500

    Update Day10.py

--- Day10.py
+++ Day10.py
@@ -1,4 +1,4 @@
-
+import matplotlib.pyplot as plt 
 
 pipe_neighbors = {
     "|": [(-1, 0), (1, 0)], 
@@ -13,13 +13,9 @@ pipe_neighbors = {
 def is_valid_pos(pos, map_size):
     return 0 <= pos[0] < map_size and 0 <= pos[1] < map_size 
 
-def print_pos_map(pos_map): 
-    for row in pos_map: 
-        print(*row)
+def get_max_pipe_dist(pipe_map):     # find the starting point 
+    pos_map = [[0 for i in range(len(pipe_map))] for x in range(len(pipe_map))] 
 
-def get_max_pipe_dist(pipe_map): 
-    pos_map = [x[:] for x in pipe_map]
-    # find the starting point 
     root_pos = None
 
     for i in range(len(pipe_map)): 
@@ -27,26 +23,39 @@ def get_max_pipe_dist(pipe_map):
             if pipe_map[i][j] == "S": 
                 root_pos = (i, j) 
 
+    # compute distance from point to point 
+                
+    
     frontier = [(root_pos, 0)]
-    visited = [] 
+    visited = {} 
 
     max_dist = 0
+    max_pos = None 
 
     while len(frontier) != 0: 
-        c_pos, c_dist = frontier.pop(0)         
+        c_pos, c_dist = frontier.pop(0)       
 
         for adj_offset in pipe_neighbors[pipe_map[c_pos[0]][c_pos[1]]]: 
             next_pos = (c_pos[0] + adj_offset[0], c_pos[1] + adj_offset[1]) 
 
-            if is_valid_pos(next_pos, len(pipe_map)) and next_pos not in visited: 
+            if is_valid_pos(next_pos, len(pipe_map)) and not visited.get(next_pos, False): 
                 if pipe_map[next_pos[0]][next_pos[1]] != ".": 
-                    frontier.append((next_pos, c_dist+1)) 
-                    max_dist = max(max_dist, c_dist+1) 
+                    frontier.append((next_pos, c_dist+1))
 
-        pos_map[c_pos[0]][c_pos[1]] = c_dist 
-        visited.append(c_pos)         
+                    if max_dist < c_dist + 1: 
+                        max_dist = c_dist + 1
+                        max_pos = next_pos 
+                    
+                    pos_map[next_pos[0]][next_pos[1]] = c_dist + 1
 
-    print_pos_map(pos_map) 
+        visited[c_pos] = True 
+
+    pos_map[root_pos[0]][root_pos[1]] = 3500 
+    pos_map[max_pos[0]][max_pos[1]] = 3500 
+
+    plt.imshow(pos_map, cmap="turbo") 
+    plt.show()
+
     return max_dist
     
 pipe_map = [list(line) for line in open("./Input/Day10.txt", "r").read().split("\n")] 
