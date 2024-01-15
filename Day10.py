907a1160677f8f093df4a10393f70b79be50665c
Evan Henry <68968256+ehenry344@users.noreply.github.com>
2024-01-14 20:49:43 -0500

    Update Day10.py

--- Day10.py
+++ Day10.py
@@ -13,35 +13,41 @@ pipe_neighbors = {
 def is_valid_pos(pos, map_size):
     return 0 <= pos[0] < map_size and 0 <= pos[1] < map_size 
 
-def get_max_pipe_dist(pipe_map): 
+def build_dist_map(input_map): 
+    dist_map = [[0 for j in range(len(input_map))] for i in range(len(input_map))]
 
-    for i in range(len(pipe_map)): #finds starting point 
-        for j in range(len(pipe_map[i])): 
-            if pipe_map[i][j] == "S": 
-                root_pos = (i, j) 
-                
-    
-    frontier = [(root_pos, 0)]
+    for i in range(len(input_map)): 
+        for j in range(len(input_map)): 
+            if input_map[i][j] == "S": root_pos = (i, j) 
+
+    frontier = [(root_pos, 0)] 
     visited = {} 
 
-    max_dist = 0
+    steps = 0 
 
     while len(frontier) != 0: 
         c_pos, c_dist = frontier.pop(0) 
 
-        if c_dist >= max_dist and not visited.get(c_pos, False): 
+        if c_dist >= steps and not visited.get(c_pos, False): 
+            steps += 1
+
             visited[c_pos] = True 
+            
+            dist_map[c_pos[0]][c_pos[1]] = c_dist
 
-            for c_offset in pipe_neighbors[pipe_map[c_pos[0]][c_pos[1]]]: 
-                check_pos = (c_pos[0] + c_offset[0], c_pos[1] + c_offset[1])
+            for n_offset in pipe_neighbors[input_map[c_pos[0]][c_pos[1]]]: 
+                n_pos = (c_pos[0] + n_offset[0], c_pos[1] + n_offset[1]) 
 
-                if is_valid_pos(check_pos, len(pipe_map)): 
-                    frontier.append((check_pos, c_dist+1))  
+                if is_valid_pos(n_pos, len(input_map)) and input_map[n_pos[0]][n_pos[1]] != ".": 
+                    frontier.append((n_pos, steps)) 
 
-                    max_dist = max(max_dist, c_dist+1) 
+    return dist_map, steps
 
-    return max_dist // 2
-    
+
 pipe_map = [list(line) for line in open("./Input/Day10.txt", "r").read().split("\n")] 
 
-print("Part 1 Solution:", get_max_pipe_dist(pipe_map)) 
+distances, steps = build_dist_map(pipe_map)
+print(steps//2) 
+
+
+    
